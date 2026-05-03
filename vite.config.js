64f082f53098dd5629a6be3f28dfd6146083fa86
owner <jerougui@github.com>
2026-05-03 11:19:46 +0200

    conf gitHub page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/speckit/',   // <-- indispensable pour GitHub Pages
   root: 'src',
   build: {
     outDir: '../dist'
